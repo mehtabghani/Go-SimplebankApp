f8bf087aadfdde82ccc7c2b421d5df3e81bc7695
mehtab <mehtab@fenix.life>
2023-05-17 18:58:57 +0500

    sqlc for tables added and unit test

---
+++ db/sqlc/main_test.go
@@ -0,0 +1,47 @@
+package db
+
+import (
+	"database/sql"
+	"log"
+	"os"
+	"testing"
+
+	_ "github.com/lib/pq"
+	// "github.com/techschool/simplebank/util"
+)
+
+const (
+	dbDrive  = "postgres"
+	dbSource = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
+)
+
+var testQueries *Queries
+var testDB *sql.DB
+
+func TestMain(m *testing.M) {
+
+	testDB, err := sql.Open(dbDrive, dbSource)
+	if err != nil {
+		log.Fatal("cannot connect to db:", err)
+	}
+
+	testQueries = New(testDB)
+
+	os.Exit(m.Run())
+}
+
+// func TestMain(m *testing.M) {
+// 	config, err := util.LoadConfig("../..")
+// 	if err != nil {
+// 		log.Fatal("cannot load config:", err)
+// 	}
+
+// 	testDB, err = sql.Open(config.DBDriver, config.DBSource)
+// 	if err != nil {
+// 		log.Fatal("cannot connect to db:", err)
+// 	}
+
+// 	testQueries = New(testDB)
+
+// 	os.Exit(m.Run())
+// }
