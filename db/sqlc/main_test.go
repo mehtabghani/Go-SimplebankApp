2c1d7babd424042555061e481fcde2b07304f468
mehtab <mehtab@fenix.life>
2023-05-25 13:41:57 +0500

    Sec 2, 6 - user golang class generated with unit DB tests and accounts Api error handling

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -11,16 +11,15 @@ import (
 	// "github.com/techschool/simplebank/util"
 )
 
-const (
-	dbDrive  = "postgres"
-	dbSource = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
-)
-
 var testQueries *Queries
 var testDB *sql.DB
 
 func TestMain(m *testing.M) {
 	config, err := util.LoadConfig("../..")
+
+	if err != nil {
+		log.Fatal("Failed to load config:", err)
+	}
 
 	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
