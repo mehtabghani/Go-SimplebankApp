b02bfa647eba70c3634da05fa78ab6013fee01db
mehtab <mehtab@fenix.life>
2023-05-26 16:43:29 +0500

    Sec 2, 21 - Implement login API with JWT and PASETO

--- api/server.go
+++ api/server.go
@@ -1,35 +1,37 @@
 package api
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
 	"github.com/go-playground/validator/v10"
 	db "github.com/mehtabghani/simplebank/db/sqlc"
+	"github.com/mehtabghani/simplebank/token"
 	"github.com/mehtabghani/simplebank/util"
-	// "github.com/mehtabghani/simplebank/token"
 	// "github.com/mehtabghani/simplebank/util"
 )
 
 // Server serves HTTP requests for our banking service.
 type Server struct {
-	// config     util.Config
-	store db.Store
-	// tokenMaker token.Maker
-	router *gin.Engine
+	config     util.Config
+	store      db.Store
+	tokenMaker token.Maker
+	router     *gin.Engine
 }
 
 // NewServer creates a new HTTP server and set up routing.
 func NewServer(config util.Config, store db.Store) (*Server, error) {
 
-	// tokenMaker, err := token.NewPasetoMaker(config.TokenSymmetricKey)
-	// if err != nil {
-	// 	return nil, fmt.Errorf("cannot create token maker: %w", err)
-	// }
+	tokenMaker, err := token.NewPasetoMaker(config.TokenSymmetricKey)
+	if err != nil {
+		return nil, fmt.Errorf("cannot create token maker: %w", err) // %w is used to wrap original error
+	}
 
 	server := &Server{
-		// config:     config,
-		store: store,
-		// tokenMaker: tokenMaker,
+		config:     config,
+		store:      store,
+		tokenMaker: tokenMaker,
 	}
 
 	// validator binding
@@ -45,7 +47,7 @@ func (server *Server) setupRouter() {
 	router := gin.Default()
 
 	router.POST("/users", server.createUser)
-	// router.POST("/users/login", server.loginUser)
+	router.POST("/users/login", server.loginUser)
 	// router.POST("/tokens/renew_access", server.renewAccessToken)
 
 	// authRoutes := router.Group("/").Use(authMiddleware(server.tokenMaker))
